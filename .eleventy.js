6455d793885fd441609959e5e891992b592733ff
Zac Heisey <zac.heisey@alextom.com>
2020-06-27 15:31:16 -0400

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,45 @@
+// Eleventy config file
+module.exports = config => {
+
+  // Set directories to pass through to the /dist folder
+  config.addPassthroughCopy( './src/images/' );
+
+  // Require sort-by-display-order utility function
+  const sortByDisplayOrder = require( './src/utils/sort-by-display-order.js' );
+
+  // Returns work items, sorted by display order
+  config.addCollection( 'work', collection => {
+
+    return sortByDisplayOrder( collection.getFilteredByGlob( './src/work/*.md' ) );
+
+  });
+
+  // Returns work items, sorted by display order then filtered by featured
+  config.addCollection( 'featuredWork', collection => {
+
+    return sortByDisplayOrder( collection.getFilteredByGlob( './src/work/*.md' ) ).filter(
+      x => x.data.featured
+    );
+
+  });
+
+  // Returns a collection of blog posts in reverse date order
+  config.addCollection( 'blog', collection => {
+
+    return [ ...collection.getFilteredByGlob( './src/posts/*.md' ) ].reverse();
+
+  });
+
+  return {
+
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
+
+  };
+
+};
